8b142c26a5f143f68606d453004457d4b1d7155d
Charles Butler <cdbutle@outlook.com>
2020-03-13 16:10:16 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,13 @@
 def my_each(collection)
-collection.yield do |i|
-  puts i
+  i=0 
+  
+  while i < collection.length 
+  yield array[i]
+  i = i + 1 
 end
+array
+end
+
+my_each(["1", "2", "3", "4"]) do |num|
+  puts "#{num}"
 end
